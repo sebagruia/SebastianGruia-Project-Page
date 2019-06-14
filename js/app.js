27f4de1a319d1b6ffd52fb69c1bdea29ab94fa2d
sebagruia <sebastiangruia@gmail.com>
2019-06-14 14:38:56 +0200

    Fixed some REsponsive Design issues
    Fixed the presentation of the web-developer div on different devices

--- js/app.js
+++ js/app.js
@@ -26,20 +26,5 @@ $('.js--scroll-to-home').click(function () {
 
     });
 
-
-/*For the sticky navigation*/
-
-    $('.js--section-features').waypoint(function (direction) {
-        if (direction == "down") {
-            $('nav').addClass('sticky');
-
-
-        } else {
-            $('nav').removeClass('sticky');
-
-
-        }
-    }, {
-        offset: '60px;'
-    });
 });
+
