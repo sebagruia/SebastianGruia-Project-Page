525178916d6b9303ad8734cbd11334fe4967c04c
Sebastian Gruia <34604352+sebagruia@users.noreply.github.com>
2019-06-30 00:10:31 +0300

    Contact Arrows. Section Order changed
    Added eventListeners to the arrows from the Contact form. Now this are clickable as well.
    Also I've changed the order of appearence of Photography and Front-end developer sections.

--- js/app.js
+++ js/app.js
@@ -32,17 +32,28 @@ document.addEventListener('DOMContentLoaded', (ev1)=>{
     console.log('Loaded the DOM');
     const up = document.querySelector('.move-up');
     const down = document.querySelector('.move-down');
-    const contactDown = document.querySelector('.contact-down');
+    const doubleDown = document.querySelectorAll('.double-down');
+    const doubleUp = document.querySelectorAll('.double-up');
     const contactUp = document.querySelector('.contact-up');
     
         up.addEventListener('click', ()=>{
                 contactUp.setAttribute('style', 'bottom:0px; transition: bottom 0.8s; -webkit-transition:bottom 0.8s; transition-timing-function:ease; -webkit-transition-timing-function:ease;');
         });
+       
 
         down.addEventListener('click', ()=>{
                 contactUp.setAttribute('style', 'bottom:-1000px; transition: bottom 0.8s; -webkit-transition:bottom 0.8s; transition-timing-function:ease; -webkit-transition-timing-function:ease;');
         });
+        
+        for(let i=0; i<2;i++){
+            doubleUp[i].addEventListener('click', ()=>{
+                contactUp.setAttribute('style', 'bottom:0px; transition: bottom 0.8s; -webkit-transition:bottom 0.8s; transition-timing-function:ease; -webkit-transition-timing-function:ease;');
+        });
+        doubleDown[i].addEventListener('click', ()=>{
+            contactUp.setAttribute('style', 'bottom:-1000px; transition: bottom 0.8s; -webkit-transition:bottom 0.8s; transition-timing-function:ease; -webkit-transition-timing-function:ease;');
+    });
 
+        }
   
    
 
